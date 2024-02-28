0f2a84d17f961addf4dcbc27161505396b7df332
Atif Rehman <atifrehman401@gmail.com>
2024-02-28 04:30:52 +0000

    js assignment

---
+++ app.js
@@ -0,0 +1,23 @@
+// alert
+alert("Hello js")
+
+// prompt
+var num1 = prompt("1");
+
+// variable
+var firstname = ("AtifRehman");
+var age = ("18");
+console.log(firstname +" " +age)
+
+// Numbers
+
+var total = (500);
+var num1 = (80);
+var num2 = (50);
+var num3 = (70);
+var num4 = (100);
+var num5 = (60);
+var num6 = (80);
+console.log(total);
+console.log(num1+num2+num3+num4+num5+num6);
+
